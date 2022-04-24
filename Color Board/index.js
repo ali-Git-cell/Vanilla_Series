024278ba4bae40cebbce875bb82ca12791e5ef38
Ali Hasham <60347508+ali-Git-cell@users.noreply.github.com>
2022-04-24 17:24:14 +0200

    style changed on mouseover and mouseout

--- Color Board/index.js
+++ Color Board/index.js
@@ -1,27 +1,33 @@
 const container = document.getElementById("container");
 const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
-const SQUARES = 500;
+const SQUARES = 600;
 
 for (let i = 0; i < SQUARES; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
 
-  square.addEventListener("mouseover", () => setColor(square));
+  square.addEventListener("mouseover", () => {
+    setColor(square);
+  });
 
-  square.addEventListener("mouseout", () => removeColor(square));
+  square.addEventListener("mouseout", () => {
+    removeColor(square);
+  });
 
   container.appendChild(square);
 }
 
-function setColor(element) {
+function setColor(ele) {
   const color = getRandomColor();
-  element.style.background = color;
-  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
-}
+  ele.style.backgroundColor = color;
+  ele.style.borderRadius = "40px";
 
-function removeColor(element) {
-  element.style.background = "#1d1d1d";
-  element.style.boxShadow = "0 0 2px #000";
+  ele.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+function removeColor(ele) {
+  ele.style.backgroundColor = "#1d1d1d";
+  ele.style.boxShadow = "0 0 2px #000";
+  ele.style.borderRadius = "0px";
 }
 
 function getRandomColor() {
